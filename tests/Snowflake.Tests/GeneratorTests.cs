1b2b4fe0f4e5ce2614653eb55a34ea8eacc274b8
Anton Vorontsov <dealwithbotalfred@gmail.com>
2025-02-22 04:44:52 +0300

    Updated to dotnet 9

--- tests/Snowflake.Tests/GeneratorTests.cs
+++ tests/Snowflake.Tests/GeneratorTests.cs
@@ -1,6 +1,5 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Collections.Concurrent;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Xunit;
@@ -10,57 +9,25 @@ namespace Snowflake.Tests;
 public sealed class GeneratorTests
 {
     [Fact]
-    public void Generate_MultipleParallelActions_ShouldNotGenerateCollisions()
+    public void Generate_ShouldNotGenerateCollisions()
     {
         var generator = new IdentifierGenerator(
             new IdentifierGeneratorConfiguration(DateTimeOffset.UtcNow.AddDays(-1)),
             datacenterId: 1,
             machineId: 1);
 
-        const int parallelism = 15;
-        const int numberOfIdentifiers = 1_000_000;
+        const int parallelism = 25;
+        const int numberOfIdentifiers = 10_000_000;
 
-        var identifierStorage = Enumerable.Range(0, parallelism).Select(_ => new List<long>()).ToArray();
-        var actions = identifierStorage.Select<List<long>, Action>(storage => () =>
+        var identifierStorage = new ConcurrentDictionary<long, byte>();
+        Parallel.For(0, numberOfIdentifiers, new ParallelOptions
         {
-            for (var i = 0; i < numberOfIdentifiers; i++)
-            {
-                storage.Add(generator.Generate());
-            }
-        }).ToArray();
-        Parallel.Invoke(new ParallelOptions { MaxDegreeOfParallelism = parallelism }, actions);
-
-        var uniqueIdentifiers = identifierStorage.SelectMany(x => x).ToHashSet();
-        uniqueIdentifiers.Count.Should().Be(parallelism * numberOfIdentifiers);
-    }
-    
-    [Fact]
-    public async Task Generate_MultipleTasks_ShouldNotGenerateCollisions()
-    {
-        var generator = new IdentifierGenerator(
-            new IdentifierGeneratorConfiguration(DateTimeOffset.UtcNow.AddDays(-1)),
-            datacenterId: 1,
-            machineId: 1);
-
-        const int parallelism = 15;
-        const int numberOfIdentifiers = 1_000_000;
-
-        var identifierStorage = Enumerable.Range(0, parallelism).Select(_ => new List<long>()).ToArray();
-
-        Task GenerateIdentifiers(IdentifierGenerator snowflakeGenerator, List<long> storage)
+            MaxDegreeOfParallelism = parallelism
+        }, _ =>
         {
-            for (var i = 0; i < numberOfIdentifiers; i++)
-            {
-                storage.Add(snowflakeGenerator.Generate());
-            }
+            identifierStorage.TryAdd(generator.Generate(), 1);
+        });
 
-            return Task.CompletedTask;
-        }
-
-        await Task.WhenAll(Enumerable.Range(0, parallelism)
-            .Select(x => GenerateIdentifiers(generator, identifierStorage[x])));
-
-        var uniqueIdentifiers = identifierStorage.SelectMany(x => x).ToHashSet();
-        uniqueIdentifiers.Count.Should().Be(parallelism * numberOfIdentifiers);
+        identifierStorage.Count.Should().Be(numberOfIdentifiers);
     }
 }
